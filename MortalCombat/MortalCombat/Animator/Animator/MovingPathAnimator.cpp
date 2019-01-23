f6d81ed5275382be0d756b680504965314b0d16b
Stelios Georgiou <stelios@oramavr.com>
2019-01-23 17:46:35 +0200

    fix ultra bugged ^ operator (replaced with pow())

--- MortalCombat/MortalCombat/Animator/Animator/MovingPathAnimator.cpp
+++ MortalCombat/MortalCombat/Animator/Animator/MovingPathAnimator.cpp
@@ -9,23 +9,23 @@ void MovingPathAnimator::Progress(timestamp_t currTime) {
 
 		if (fall) {
 			if (sprite->y < 150)
-				sprite->y += (float)sqrt((((1 - (sprite->x / 800) ^ 2))) / 10.0f) * 5;
+				sprite->y += (float)sqrt((((1 - pow((sprite->x / 800), 2)))) / 10.0f) * 10;
 			else if (sprite->y < 200)
-				sprite->y += (float)sqrt((((1 - (sprite->x / 800) ^ 2))) / 10.0f) * 15;
+				sprite->y += (float)sqrt((((1 - pow((sprite->x / 800), 2)))) / 10.0f) * 20;
 			else if (sprite->y < 250)
-				sprite->y += (float)sqrt((((1 - (sprite->x / 800) ^ 2))) / 10.0f) * 25;
+				sprite->y += (float)sqrt((((1 - pow((sprite->x / 800), 2)))) / 10.0f) * 30;
 			else
-				sprite->y += (float)sqrt((((1 - (sprite->x / 800) ^ 2))) / 10.0f) * 35;
+				sprite->y += (float)sqrt((((1 - pow((sprite->x / 800), 2)))) / 10.0f) * 40;
 		}
 		else {
 			if (sprite->y < 150)
-				sprite->y -= (float)sqrt((((1 - (sprite->x / 800) ^ 2))) / 10.0f) * 5;
+				sprite->y -= (float)sqrt((((1 - pow((sprite->x / 800), 2)))) / 10.0f) * 10;
 			else if (sprite->y < 200)
-				sprite->y -= (float)sqrt((((1 - (sprite->x / 800) ^ 2))) / 10.0f) * 15;
+				sprite->y -= (float)sqrt((((1 - pow((sprite->x / 800), 2)))) / 10.0f) * 20;
 			else if (sprite->y < 250)
-				sprite->y -= (float)sqrt((((1 - (sprite->x / 800) ^ 2))) / 10.0f) * 25;
+				sprite->y -= (float)sqrt((((1 - pow((sprite->x / 800), 2)))) / 10.0f) * 30;
 			else
-				sprite->y -= (float)sqrt((((1 - (sprite->x / 800) ^ 2))) / 10.0f) * 35;
+				sprite->y -= (float)sqrt((((1 - pow((sprite->x / 800), 2)))) / 10.0f) * 40;
 		}
 		sprite->Move(x, 0);
 
